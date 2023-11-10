a7ae668e2290e91d1b216cab89718f681fefddba
AhmadShamouka <shamoukaahmad@gmail.com>
2023-11-10 22:02:48 +0200

    adding sort function with the update needed for the code

--- script.js
+++ script.js
@@ -1,88 +1,111 @@
 const taskInput = document.getElementById('task-input');
 const addTaskButton = document.getElementById('add-task');
 const taskList = document.getElementById('tasks');
+const displayPrompt = document.getElementById('displayPromt');
+const submitButton = document.getElementById('submit-button');
+const editInput = document.getElementById('task-edit');
+const sortbtn = document.getElementById('sort-task');
+const Sortarray = [];
 
+sortbtn.addEventListener('click', sortTask);
 addTaskButton.addEventListener('click', addTask);
-let Sortarray=["ahmad","zein","ali","hadi","hilal"]
-let unsortarray=[]
-function addTask() {
-    const taskName = taskInput.value;
-    if (taskName === '') {
-        alert('Task name cannot be empty.');
-        return;
-    }
-    const li = document.createElement('li');
-    li.innerHTML = `
+
+function renderTask() {
+    taskList.innerHTML = "";
+    Sortarray.forEach((taskName, index) => {
+        const li = document.createElement('li');
+        li.innerHTML = createTaskElement(taskName, index);
+        taskList.appendChild(li);
+        addEventListeners(li, index);
+    });
+}
+
+function createTaskElement(taskName, index) {
+    return `
         <div class="ag-format-container">
             <div class="ag-courses_box">
                 <div class="ag-courses_item">
                     <a href="#" class="ag-courses-item_link">
                         <div class="ag-courses-item_bg"></div>
                         <div class="ag-courses-item_title">
-                        <h1 class="task-name">${taskName}</h1>
-                        <button id="mark-button" class="mark-button"><b>x</b></button>
+                            <h1 class="task-name">${taskName}</h1>
+                            <button class="mark-button" index="${index}"><b>x</b></button>
                         </div>
                         <div class="ag-courses-item_date-box">
                             <span class="ag-courses-item_date">
                                 <span class="task-buttons">
-                                    <button class="edit-button">Edit</button>
-                                    <button class="delete-button">Delete</button>
-                                    <button class="sort-button">Sort</button>
+                                    <button class="edit-button" index="${index}">Edit</button>
+                                    <button class="delete-button" index="${index}">Delete</button>
+                                   
                                 </span>
                             </span>
                         </div>
                     </a>
                 </div>
             </div>
-        </div>
-    `;
-    taskList.appendChild(li);
+        </div>`
+        ;
+}
 
-        
-    const markdone = document.getElementById('mark-button');
-    
-    markdone.addEventListener('click', mark);    
-    function mark() {
-        var x = document.querySelector(".mark-button");
-        if (x.innerHTML === "✔") {
-            x.innerHTML = "<b>x</b>";
-            x.style.backgroundColor = "red";
-        } else {
-            x.innerHTML = "✔";
-            x.style.backgroundColor = "Green"; 
-        }
-      }
+function addTask() {
+    const taskName = taskInput.value;
+    if (taskName === '') {
+        alert('Task name cannot be empty.');
+        return;
+    }
+
+    Sortarray.push(taskName);
+    renderTask();
+    taskInput.value = '';
+}
+function sortTask(){
+    Sortarray.sort();
+    renderTask();
+}
+
+function addEventListeners(li, index) {
+    const markButton = li.querySelector('.mark-button');
+    markButton.addEventListener('click', () => mark(index));
 
     const deleteButton = li.querySelector('.delete-button');
-    
-    deleteButton.addEventListener('click', function() {
-        li.remove();
-    });
-    const editButton=li.querySelector('.edit-button');
-    const displayPromt=document.getElementById('displayPromt')
-    editButton.addEventListener('click', function() {
-        displayPromt.style.display = 'block';
-        const submitButton = document.getElementById('submit-button');
-        const editInput = document.getElementById('task-edit');    
+    deleteButton.addEventListener('click', () => deleteTask(li));
 
-        editInput.value = taskName;
-    
-        submitButton.addEventListener('click', function() {
-            const editedTaskName = editInput.value;
-            const taskNameSpan = li.querySelector('.task-name');
-            taskNameSpan.textContent = editedTaskName;
-            displayPromt.style.display = 'none';
-        });
-    });
-    
-    taskInput.value = '';
-
-
-
-
+    const editButton = li.querySelector('.edit-button');
+    editButton.addEventListener('click', () => editTask(li, Sortarray[index]));
 }
+
+function mark(index) {
+    const markButton = document.querySelector(`.mark-button[index="${index}"]`);
+    if (markButton.innerHTML === "✔") {
+        markButton.innerHTML = "<b>x</b>";
+        markButton.style.backgroundColor = "red";
+    } else {
+        markButton.innerHTML = "✔";
+        markButton.style.backgroundColor = "green";
+    }
+}
+
+function deleteTask(listItem) {
+    listItem.remove();
+}
+
+function editTask(listItem, originalTaskName) {
+    displayPrompt.style.display = 'block';
+    editInput.value = originalTaskName;
+
+    submitButton.addEventListener('click', function () {
+        const editedTaskName = editInput.value;
+        const taskNameSpan = listItem.querySelector('.task-name');
+        taskNameSpan.textContent = editedTaskName;
+        displayPrompt.style.display = 'none';
+    });
+}
+
 taskInput.addEventListener('keypress', (event) => {
     if (event.key === 'Enter') {
         addTask();
     }
 });
+
+// Initial rendering
+renderTask();
